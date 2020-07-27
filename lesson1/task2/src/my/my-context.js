045ab7a5d682e17a65e1762cc1429b6553b6fd11
Milkweedd <antontab91@gmail.com>
2020-07-27 15:50:01 +0300

    sa

--- lesson1/task2/src/my/my-context.js
+++
@@ -1,7 +0,0 @@
-import React, { createContext } from "react";
-
-export const myName = { firstName: "Tony", lastName: "Abramovych" };
-export const myContext = React.createContext();
-
-//1.Создаем переменную с данными для контекста - экспорт
-//2.Создаем контекст - новую переменную  и присваиваем ей значение React.createContext() - экспорт
