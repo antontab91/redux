3df65caa8f72599009816c2d2c3c5735c6a0c52d
Milkweedd <antontab91@gmail.com>
2020-08-10 03:16:48 +0300

    sa

--- lesson1/task2/src/UserMenu.jsx
+++ lesson1/task2/src/UserMenu.jsx
@@ -11,7 +11,7 @@ class UserMenu extends React.Component {
     render() {
         console.log(this.context)
         return (
-            <div className="menu">
+            <div className="menu" {...this.props}>
                 <span className="menu__greeting">{`Hello, ${this.context.name}`}</span>
                 <img alt="User Avatar" src={this.context.avatar_url} className="menu__avatar" />
             </div>
