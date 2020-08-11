153fc4110c7e555ababfc81c94587cf3e6d9212d
Milkweedd <antontab91@gmail.com>
2020-08-11 05:16:59 +0300

    ds

---
+++ lesson2/task2/src/index.js
@@ -0,0 +1,25 @@
+import './index.scss';
+import store, { increment, decrement, reset } from './store';
+
+const resultElem = document.querySelector('.counter__result');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const resetBtn = document.querySelector('[data-action="reset"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+
+const onIncrement = () => {
+  store.dispatch(increment());
+}
+const onIDecrement = () => {
+  store.dispatch(decrement());
+}
+const onReset = () => {
+  store.dispatch(reset());
+}
+
+incrementBtn.addEventListener('click', onIncrement);
+decrementBtn.addEventListener('click', onIDecrement);
+resetBtn.addEventListener('click', onReset);
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
