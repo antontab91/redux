235308e59720f151bc979b56f2308703a794e1d0
Milkweedd <antontab91@gmail.com>
2020-08-11 03:17:31 +0300

    sa

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -1,36 +1,35 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
-const INCREMENT = 'COUNTER/INCREMENT';
-const DECREMENT = 'COUNTER/DECREMENT';
+const INCREMENT = "COUNTER/INCREMENT";
+const DECREMENT = "COUNTER/DECREMENT";
 
-export const increment = () => {
+const increment = () => {
   return {
     type: INCREMENT,
-  }
-}
+  };
+};
 
-export const decrement = () => {
+const decrement = () => {
   return {
     type: DECREMENT,
-  }
-}
+  };
+};
 
-export const counterReducer = (state = 0, action) => {
+const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case INCREMENT:
-      return state = state + 1;
+      return state + 1;
+
     case DECREMENT:
-      return state = state - 1;
+      return state - 1;
+
     default:
       return state;
   }
-}
+};
 
 const store = createStore(counterReducer);
-
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-
-console.log(store.getState());
+// store.dispatch(increment());
+// store.dispatch(increment());
+// console.log(store.getState());
+export { increment, decrement, store };
