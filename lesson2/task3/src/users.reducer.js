d00a0f2ba09f99522b687a0d7c3ba05e28e9f5eb
Milkweedd <antontab91@gmail.com>
2020-08-12 05:55:46 +0300

    sd

--- lesson2/task3/src/users.reducer.js
+++ lesson2/task3/src/users.reducer.js
@@ -1,2 +1,31 @@
-import { createStore } from 'redux';
+import { addUser, deleteUser } from './users.actions';
 
+const initialState = {
+  usersList: [],
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_USER":
+      return Object.assign({}, state, {
+        usersList: [
+          ...state.usersList,
+          {
+            id: action.user.id,
+            name: action.user.name,
+          }
+        ]
+      })
+
+    case "DELETE_USER":
+      const updateUsersList = state.usersList.filter((el) => {
+        return el.id !== action.id;
+      })
+      return {
+        usersList: updateUsersList,
+      }
+
+    default:
+      return state
+  }
+}
