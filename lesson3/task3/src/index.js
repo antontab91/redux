abe9287f30cd31c7d239cdc59a6f2f85a819a7de
Milkweedd <antontab91@gmail.com>
2020-08-14 05:39:35 +0300

    ds

--- lesson3/task3/src/index.js
+++ lesson3/task3/src/index.js
@@ -1,15 +1,27 @@
-import store from './store';
-import { setLanguage } from './language.actions';
+// import store from './store';
+// import { setLanguage } from './language.actions';
 
 
-store.subscribe(() => {
-  console.log(store.getState())
-})
+// store.subscribe(() => {
+//   console.log(store.getState())
+// })
 
 
-store.dispatch(setLanguage('ru'));
-store.dispatch(setLanguage('ru'));
-store.dispatch(setLanguage('en'));
+// store.dispatch(setLanguage('ru'));
+// store.dispatch(setLanguage('ru'));
+// store.dispatch(setLanguage('en'));
 
-console.log(store.getState());
+// console.log(store.getState());
 
+const ADD_PRODUCT = 'TRASH/ADD_PRODUCT';
+const REMOVE_PRODUCT = 'TRASH/REMOVE_PRODUCT';
+
+const initialTrash = {
+  trashProducts: [],
+}
+
+const cartReducer = (state = initialTrash, action) => {
+  // switch (action.type) {
+
+  // }
+}
