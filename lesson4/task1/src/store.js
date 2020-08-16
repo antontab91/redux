749fb9bd1b53518f4a6c30c84521816036bed767
Milkweedd <antontab91@gmail.com>
2020-08-16 05:28:35 +0300

    sa

--- lesson4/task1/src/store.js
+++ lesson4/task1/src/store.js
@@ -6,11 +6,11 @@ const store = createStore(counterReducer);
 
 export default store;
 
-store.subscribe(() => {
-  console.log(store.getState());
-})
+// store.subscribe(() => {
+//   console.log(store.getState());
+// })
 
-store.dispatch(increment())
-store.dispatch(increment())
-store.dispatch(decrement())
-store.dispatch(reset())
+// store.dispatch(increment())
+// store.dispatch(increment())
+// store.dispatch(decrement())
+// store.dispatch(reset())
