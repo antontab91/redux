e9dee5eb8aacdcc4db7d62d3e5c3b7205a86087d
Milkweedd <antontab91@gmail.com>
2020-08-17 07:29:08 +0300

    as

--- lesson4/task2/src/counter/counter.action.js
+++ lesson4/task2/src/counter/counter.action.js
@@ -1,1 +1,47 @@
 import { createStore } from 'redux';
+import store from '../../../task1/src/store';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'COUNTER/RESET';
+
+const increment = () => {
+  return {
+    type: INCREMENT,
+  }
+}
+const decrement = () => {
+  return {
+    type: DECREMENT,
+  }
+}
+const reset = () => {
+  return {
+    type: RESET,
+  }
+}
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return sate + 1;
+    case DECREMENT:
+      return state - 1;
+    case RESET:
+      return state = 0;
+    default:
+      return state;
+  }
+}
+
+const state = createStore(counterReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(reset());
