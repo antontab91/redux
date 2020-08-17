347af378572fe23794b36c48e38b7d90cc8ef081
Milkweedd <antontab91@gmail.com>
2020-08-18 02:53:22 +0300

    sd

--- lesson4/task3/src/users/UsersList.jsx
+++ lesson4/task3/src/users/UsersList.jsx
@@ -4,16 +4,22 @@ import User from './User';
 
 // это в этой компонетнте я конкечусь к стору 
 
-const UsersList = () => {
+const UsersList = ({ users }) => {
   const [currentPage, setPage] = useState(0);
 
+  const itemsPerPage = 3;
+  const totalItems = users.length
   const goPrev = () => {
     setPage(currentPage - 1)
   }
-
   const goNext = () => {
     setPage(currentPage + 1)
   }
+
+  const firstIndexOnCurrentPage = currentPage * itemsPerPage; //0*3 = 0 первый индекс на первой странице, 1*3 =3 первый индекс на второй странице ... и тд . и так на каждой текущей странице 
+  const lastIndexOnCurrentPage = firstIndexOnCurrentPage + 3; // +3 к каждому первому индексу = последний индекс текущей страницы 
+
+
 
   return (
     <div>
@@ -21,9 +27,18 @@ const UsersList = () => {
         currentPage={currentPage}
         goNext={goNext}
         goPrev={goPrev}
+        itemsPerPage={itemsPerPage}
+        totalItems={totalItems}
+
       />
       <ul>
-        <User />
+        {
+          users.slice(firstIndexOnCurrentPage, lastIndexOnCurrentPage).map((user) => {
+            return (
+              <User key={user.id} name={user.name} age={user.age} />
+            )
+          })
+        }
       </ul>
     </div>
   )
