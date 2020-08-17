d0bad44c4e2284f9de1ef9bbb79ae419573cc420
Milkweedd <antontab91@gmail.com>
2020-08-17 07:30:42 +0300

    ds

--- lesson4/task2/src/index.jsx
+++ lesson4/task2/src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import './users/users.reducer'
+import './users/users.reducer';
+import './counter/counter.action';
 
 const root = document.querySelector('#root');
 
