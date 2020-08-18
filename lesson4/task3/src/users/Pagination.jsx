1965a4079fcb89e676b65729f9899c403e0b67af
Milkweedd <antontab91@gmail.com>
2020-08-18 04:17:10 +0300

    sa

--- lesson4/task3/src/users/Pagination.jsx
+++ lesson4/task3/src/users/Pagination.jsx
@@ -1,17 +1,50 @@
-import React from 'react';
+// import React from 'react';
+
+// const Pagination = ({ currentPage, goPrev, goNext, totalItems, itemsPerPage }) => {
+
+//   const isPrevPageAvailable = currentPage === 0;
+//   const isNextPageAvailable = currentPage === Math.ceil(totalItems / itemsPerPage) - 1;
+
+//   return (
+//     <div className="pagination">
+//       <button onClick={goPrev} disabled={isPrevPageAvailable} className="btn">{isPrevPageAvailable ? "←" : ""}</button>
+//       <span className="pagination__page">{currentPage + 1}</span>
+//       <button onClick={goNext} className="btn">{isNextPageAvailable ? "→" : ""}</button>
+//     </div>
+
+
+//   )
+// }
+
+// export default Pagination;
+
+
+
+import React from "react";
 
 const Pagination = ({ currentPage, goPrev, goNext, totalItems, itemsPerPage }) => {
-
-  const isPrevPageAvailable = (currentPage + 1) !== 1;
-  const isNextPageAvailable = (currentPage + 1) * 3 < totalItems;
+  const isPrevPageAvailable = currentPage + 1 !== 1;
+  const isNextPageAvailable = (currentPage + 1) * itemsPerPage < totalItems;
 
   return (
     <div className="pagination">
-      <button onClick={goPrev} className="btn">{isPrevPageAvailable ? "←" : ""}</button>
+      <button
+        className="btn"
+        onClick={goPrev}
+        disabled={!isPrevPageAvailable}
+      >
+        {isPrevPageAvailable && "←"}
+      </button>
       <span className="pagination__page">{currentPage + 1}</span>
-      <button onClick={goNext} className="btn">{isNextPageAvailable ? "→" : ""}</button>
+      <button
+        className="btn"
+        onClick={goNext}
+        disabled={!isNextPageAvailable}
+      >
+        {isNextPageAvailable && "→"}
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Pagination;
