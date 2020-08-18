73631d5bdcc7ca6ea2f7c0dc497862f970efd337
Milkweedd <antontab91@gmail.com>
2020-08-18 05:15:09 +0300

    ds

--- lesson4/task3/src/store.js
+++ lesson4/task3/src/store.js
@@ -2,15 +2,20 @@ import { createStore } from 'redux';
 import userReducer from './users/user.reducer';
 import { goNext, goPrev } from './users/user.actions';
 
-const store = createStore(userReducer);
+const store = createStore(
+  userReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-store.subscribe(() => {
-  console.log(store.getState());
-})
+export default store;
+
+// store.subscribe(() => {
+//   console.log(store.getState());
+// })
 
 
-store.dispatch(goPrev())
-store.dispatch(goPrev())
-store.dispatch(goPrev())
-store.dispatch(goPrev())
-store.dispatch(goNext())
+// store.dispatch(goPrev())
+// store.dispatch(goPrev())
+// store.dispatch(goPrev())
+// store.dispatch(goPrev())
+// store.dispatch(goNext())
