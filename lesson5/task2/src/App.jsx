0432c590bc3781d4899568e16ffd64a1ceb91765
Milkweedd <antontab91@gmail.com>
2020-08-20 02:50:49 +0300

    as

--- lesson5/task2/src/App.jsx
+++ lesson5/task2/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TransferList from './options/TransferList';
 
 const App = () => {
   return (
--- lesson5/task2/src/index.jsx
+++ lesson5/task2/src/index.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'index.scss';
+import './index.scss';
 import App from './App';
+
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(<App />, root);
