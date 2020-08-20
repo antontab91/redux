cf03f3c2b56c302cad5fec7a80ff5acb6295dbe5
Milkweedd <antontab91@gmail.com>
2020-08-20 03:08:59 +0300

    as

--- lesson5/task2/src/App.jsx
+++ lesson5/task2/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import TransferList from './options/TransferList';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <TransferList />
+    <Provider store={store} >
+      <TransferList />
+    </Provider>
   )
 }
 
