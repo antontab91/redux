acfdbc4f943db19ad8f7ba6f67660d90ef1e0f05
Milkweedd <antontab91@gmail.com>
2020-08-20 14:24:56 +0300

    ds

--- lesson5/task3/src/users/user.selectors.js
+++
@@ -1,6 +0,0 @@
-export const userSelector = state => {
-  const allUsers = state.users.usersList;
-  const filterText = state.users.filterText
-  return allUsers.filter((user) =>
-    user.name.toLowerCase().includes(filterText.toLowerCase()));
-}
