99441333a76542a4811c3f6fda349e19e7c43fd0
Milkweedd <antontab91@gmail.com>
2020-08-22 01:10:12 +0300

    df

--- lesson5/task3/src/users/User.jsx
+++ lesson5/task3/src/users/User.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const User = ({ name, age }) => {
   return (
     <li className="user">
-      <span className="user__name">Tad</span>
-      <span className="user__age">18</span>
+      <span className="user__name">{name}</span>
+      <span className="user__age">{age}</span>
     </li>
   )
 }
