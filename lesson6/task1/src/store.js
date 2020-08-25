7bc7772ce1c53e7f34af6bfab00075c312d21fdf
Milkweedd <antontab91@gmail.com>
2020-08-26 02:59:27 +0300

    sd

--- lesson6/task1/src/store.js
+++ lesson6/task1/src/store.js
@@ -1,13 +1,27 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import counterReducer from './counter/counter.reducer';
 
 const storeCombiner = combineReducers({
   counter: counterReducer,
 })
 
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info(action);
+  const result = next(action);
+  console.log('new state', store.getState())
+  console.groupEnd();
+  return result;
+}
+
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   storeCombiner,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
+
 )
 
 export default store;
