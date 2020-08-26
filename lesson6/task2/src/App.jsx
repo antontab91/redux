d05cb66003a5293f060ecefc85cf1974458783ee
Milkweedd <antontab91@gmail.com>
2020-08-27 02:54:17 +0300

    ds

---
+++ lesson6/task2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import UserInfo from '../users/UserInfo';
+import SearchField from '../users/SearchField';
+
+const App = () => {
+  return (
+    <div className="page">
+      <UserInfo />
+      <SearchField />
+      {/* <!-- UserInfo --> */}
+      {/* <!-- Spinner --> */}
+      {/* <!-- SearchField --> */}
+    </div>
+  )
+}
+
+export default App;
