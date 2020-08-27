52a0dd162ca11ec2f98afea76635beb598eb5251
Milkweedd <antontab91@gmail.com>
2020-08-27 05:05:44 +0300

    as

--- lesson6/task2/users/SearchField.jsx
+++ lesson6/task2/users/SearchField.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import getUserData from './users.gateway';
 
-const SearchField = () => {
-  return (
-    <div className="name-form">
-      <input type="text" className="name-form__input" />
-      <button className="name-form__btn btn">Show</button>
-    </div>
-  )
+class SearchField extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userData: '',
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      userData: e.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div className="name-form" >
+        <input type="text" className="name-form__input" onChange={this.handleChange} value={this.state.userData} />
+        <button className="name-form__btn btn">Show</button>
+      </div>
+    )
+  }
 }
 
 export default SearchField;
