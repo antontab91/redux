62da852c579fb999c27a358e5114ef15dc87df68
Milkweedd <antontab91@gmail.com>
2020-09-02 01:25:18 +0300

    ds

--- lesson6/task3/src/weather/Weather.jsx
+++ lesson6/task3/src/weather/Weather.jsx
@@ -1,29 +1,46 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import { fetchWeatherData } from './weather.actions';
+import { useEffect } from 'react';
 
-const Weather = () => {
+const Weather = ({ state, fetchWeatherData }) => {
+  useEffect(() => {
+    fetchWeatherData()
+  }, []);
+
+  console.log(state);
+
   return (
     <main className="weather">
       <h1 className="weather__title">Weather data</h1>
       <ul className="cities-list">
-        {/* <li className="city">
-          <span className="city__name">Lake Hilmaside</span>
-          <span className="city__temperature">78 F</span>
-        </li>
-        <li className="city">
-          <span className="city__name">Lake Minnieberg</span>
-          <span className="city__temperature">8 F</span>
-        </li>
-        <li className="city">
-          <span className="city__name">East Gerhardshire</span>
-          <span className="city__temperature">23 F</span>
-        </li> */}
+        {state.map(({ id, name, temperature }) => {
+          return (
+            <li key={id} className="city">
+              <span className="city__name">{name}</span>
+              <span className="city__temperature">{`${temperature} F`}</span>
+            </li>
+          )
+        })}
+
 
       </ul>
     </main>
   )
 }
 
+const mapState = state => {
+  return {
+    state: state.weather.weatherData,
+  }
+}
+
+const mapDispatch = {
+  fetchWeatherData,
+}
+
+const connector = connect(mapState, mapDispatch);
+const connectedWeather = connector(Weather);
 
 
-export default Weather;
+export default connectedWeather;
